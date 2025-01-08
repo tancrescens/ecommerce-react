3ca939c1a9776d9260e409fcea1c51a38dd0ce88
crescens tan <tancrescens@gmail.com>
2025-01-08 07:55:00 +0000

    Created ProductPage and RegisterPage pages

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -9,6 +9,9 @@ export default function App() {
   return (
     <>
       <Navbar />
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
       <HomePage />
       <Footer/>
     </>
---
+++ ecommerce-react/src/Pages/ProductPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ProductsPage() {
+  return (
+    <div className="container mt-5">
+      <h1>Our Products</h1>
+      <p>This is where we'll display our product catalog.</p>
+    </div>
+  );
+}
