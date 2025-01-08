dee4e97c2c02e9fadd2e324cfc3b186314c1c1c0
crescens tan <tancrescens@gmail.com>
2025-01-08 07:45:37 +0000

    refactored navbar

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -1,48 +1,13 @@
 import React, { useState } from 'react'
 import './styles.css';
 import ProductCard from "./Components/ProductCard.jsx"
+import Navbar from "./Components/Navbar.jsx"
 
 export default function App() {
-
-  const [isNavbarShowing, setNavbarShowing] = useState(false);
-    
-  // Toggle the collapse state
-  const toggleNavbar = () => {
-      setNavbarShowing(!isNavbarShowing);
-  };
   
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <a className="navbar-brand" href="#">E-Shop</a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={toggleNavbar}
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className={`collapse navbar-collapse ${isNavbarShowing ? "show" : ""}`}
-            id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Products</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+    <Navbar/>
       <header className="bg-primary text-white text-center py-5">
         <div className="container">
           <h1 className="display-4">Welcome to E-Shop</h1>
