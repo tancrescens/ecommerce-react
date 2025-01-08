d6f30c1642d9dbae27eb3fc6dcfe665cc2bfcdcc
crescens tan <tancrescens@gmail.com>
2025-01-08 07:51:46 +0000

    Added HomePage Page and Footer component

---
+++ ecommerce-react/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (<>
+        <footer className="bg-dark text-white text-center py-3">
+            <div className="container">
+                <p>&copy; 2025 E-Shop. All rights reserved.</p>
+            </div>
+        </footer>
+    </>)
+}
