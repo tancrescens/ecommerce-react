ec014819e78ff8b09253b821702beed0bb4ebceb
crescens tan <tancrescens@gmail.com>
2025-01-08 07:59:35 +0000

    front-end routing

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
+import {Switch, Route, Link} from 'wouter';
 import './styles.css';
 import Navbar from "./Components/Navbar.jsx"
 import HomePage from './Pages/HomePage.jsx';
 import Footer from './Components/Footer.jsx'
+import ProductsPage from './Pages/ProductPage.jsx';
+import RegisterPage from './Pages/RegisterPage.jsx';
 
 export default function App() {
 
@@ -11,8 +14,9 @@ export default function App() {
       <Navbar />
       <Switch>
         <Route path="/" component={HomePage} />
+        <Route path="/products" component={ProductsPage}/>
+        <Route path="/register" component={RegisterPage}/>
       </Switch>
-      <HomePage />
       <Footer/>
     </>
   )
