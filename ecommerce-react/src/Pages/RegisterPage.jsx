2a8a054a19dbbfd4b39295a53a92e710afd054d2
crescens tan <tancrescens@gmail.com>
2025-01-09 09:59:59 +0000

    Formik used

--- ecommerce-react/src/Pages/RegisterPage.jsx
+++ ecommerce-react/src/Pages/RegisterPage.jsx
@@ -1,66 +1,162 @@
 import React from 'react';
+import { Formik, Field, Form } from 'formik';
 
 export default function RegisterPage() {
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    salutation: '',
+    marketingPreferences: [],
+    country: ''
+  };
+
+  const handleSubmit = (values, formikHelpers) => {
+    // Here you would typically make an API call to register the user
+    console.log('Form values:', values);
+    formikHelpers.setSubmitting(false);
+  };
+
   return (
     <div className="container mt-5">
       <h1>Register</h1>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
-          <input type="text" className="form-control" id="name" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" id="password" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
-          <input type="password" className="form-control" id="confirmPassword" />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Salutation</label>
-          <div>
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="salutation" id="mr" value="Mr" />
-              <label className="form-check-label" htmlFor="mr">Mr</label>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        {(formik) => (
+          <Form>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">Name</label>
+              <Field
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+              />
             </div>
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="salutation" id="ms" value="Ms" />
-              <label className="form-check-label" htmlFor="ms">Ms</label>
+
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email</label>
+              <Field
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+              />
             </div>
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="radio" name="salutation" id="mrs" value="Mrs" />
-              <label className="form-check-label" htmlFor="mrs">Mrs</label>
+
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Password</label>
+              <Field
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+              />
             </div>
-          </div>
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Marketing Preferences</label>
-          <div className="form-check">
-            <input className="form-check-input" value="marketing" type="checkbox" id="emailMarketing" />
-            <label className="form-check-label" htmlFor="emailMarketing">Email Marketing</label>
-          </div>
-          <div className="form-check">
-            <input className="form-check-input" value="sms" type="checkbox" id="smsMarketing" />
-            <label className="form-check-label" htmlFor="smsMarketing">SMS Marketing</label>
-          </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="country" className="form-label">Country</label>
-          <select className="form-select" id="country">
-            <option value="">Select Country</option>
-            <option value="sg">Singapore</option>
-            <option value="my">Malaysia</option>
-            <option value="in">Indonesia</option>
-            <option value="th">Thailand</option>
-          </select>
-        </div>
-        <button type="submit" className="btn btn-primary">Register</button>
-      </form>
+
+            <div className="mb-3">
+              <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+              <Field
+                type="password"
+                className="form-control"
+                id="confirmPassword"
+                name="confirmPassword"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">Salutation</label>
+              <div>
+                <div className="form-check form-check-inline">
+                  <Field
+                    className="form-check-input"
+                    type="radio"
+                    name="salutation"
+                    id="mr"
+                    value="Mr"
+                  />
+                  <label className="form-check-label" htmlFor="mr">Mr</label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <Field
+                    className="form-check-input"
+                    type="radio"
+                    name="salutation"
+                    id="ms"
+                    value="Ms"
+                  />
+                  <label className="form-check-label" htmlFor="ms">Ms</label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <Field
+                    className="form-check-input"
+                    type="radio"
+                    name="salutation"
+                    id="mrs"
+                    value="Mrs"
+                  />
+                  <label className="form-check-label" htmlFor="mrs">Mrs</label>
+                </div>
+              </div>
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">Marketing Preferences</label>
+              <div className="form-check">
+                <Field
+                  className="form-check-input"
+                  type="checkbox"
+                  id="emailMarketing"
+                  name="marketingPreferences"
+                  value="email"
+                />
+                <label className="form-check-label" htmlFor="emailMarketing">
+                  Email Marketing
+                </label>
+              </div>
+              <div className="form-check">
+                <Field
+                  className="form-check-input"
+                  type="checkbox"
+                  id="smsMarketing"
+                  name="marketingPreferences"
+                  value="sms"
+                />
+                <label className="form-check-label" htmlFor="smsMarketing">
+                  SMS Marketing
+                </label>
+              </div>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="country" className="form-label">Country</label>
+              <Field
+                as="select"
+                className="form-select"
+                id="country"
+                name="country"
+              >
+                <option value="">Select Country</option>
+                <option value="sg">Singapore</option>
+                <option value="my">Malaysia</option>
+                <option value="in">Indonesia</option>
+                <option value="th">Thailand</option>
+              </Field>
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={formik.isSubmitting}
+            >
+              Register
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
